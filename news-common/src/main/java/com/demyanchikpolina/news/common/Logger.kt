ca300d3b79eefcf153bf8a93fbacb8a8f41bfe4f
demyanchikpolina <p.demyanchik@gmail.com>
2024-06-06 13:58:12 +0200

    Fix detekt errors

--- news-common/src/main/java/com/demyanchikpolina/news/common/Logger.kt
+++ news-common/src/main/java/com/demyanchikpolina/news/common/Logger.kt
@@ -3,20 +3,30 @@ package com.demyanchikpolina.news.common
 import android.util.Log
 
 interface Logger {
+    fun d(
+        tag: String,
+        message: String
+    )
 
-    fun d(tag: String, message: String)
-
-    fun e(tag: String, message: String)
+    fun e(
+        tag: String,
+        message: String
+    )
 }
 
-fun androidLogcatLogger() = object : Logger {
+fun androidLogcatLogger() =
+    object : Logger {
+        override fun d(
+            tag: String,
+            message: String
+        ) {
+            Log.d(tag, message)
+        }
 
-    override fun d(tag: String, message: String) {
-        Log.d(tag, message)
+        override fun e(
+            tag: String,
+            message: String
+        ) {
+            Log.e(tag, message)
+        }
     }
-
-    override fun e(tag: String, message: String) {
-        Log.e(tag, message)
-    }
-
-}
